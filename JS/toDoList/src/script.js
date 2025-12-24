243026b4c65dd35df8dde69d6c0ba344d97e94e5
Madush <madushdamith8@gmail.com>
2025-12-24 20:19:07 +0530

    Modified the Todo List with a modern UI

--- JS/toDoList/src/script.js
+++ JS/toDoList/src/script.js
@@ -1,6 +1,7 @@
 const todoInput = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
-const todoList = document.getElementById("todoList");
+const todoList = document.getElementById("toDoList");
+const emptyState = document.getElementById("emptyState");
 
 function addTask() {
   const taskText = todoInput.value.trim();
@@ -15,14 +16,18 @@ function addTask() {
     "flex justify-between items-center bg-gray-100 px-3 py-2 rounded-xl";
 
   li.innerHTML = `
-    <span>${taskText}</span>
-    <button class="text-red-500 hover:text-red-700 font-bold">✕</button>
-  `;
+<span>${taskText}</span>
+<button class = "text-red-500 hover:text-red-700 font-bold">X</button>`;
 
   li.querySelector("button").addEventListener("click", () => {
     li.remove();
+
+    if (todoList.children.length === 0) {
+      emptyState.style.display = "flex";
+    }
   });
 
+  emptyState.style.display = "none";
   todoList.appendChild(li);
 
   todoInput.value = "";
